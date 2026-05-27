8f02d4746682c0347fb8be6ea13cf7c852446134
HmerMza <hmermzag@gmail.com>
2026-05-27 00:09:16 -0500

    feat: add validation for login and register field

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -2,18 +2,18 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from config.database import get_db
 from controllers.user_controller import UserController
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 class UserRegister(BaseModel):
-    username: str
-    name: str
-    password: str
+    username: str = Field(..., min_length=4)
+    name: str = Field(..., min_length=4)
+    password: str = Field(..., min_length=6)
 
 class UserLogin(BaseModel):
-    username: str
-    password: str
+    username: str = Field(..., min_length=1)
+    password: str = Field(..., min_length=1)
 
 @router.post("/register")
 def register(user: UserRegister, db: Session = Depends(get_db)):
